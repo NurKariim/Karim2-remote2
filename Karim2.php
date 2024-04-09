2a536eaee54d637cf5ebbdb92da3450d341f5365
Nur Karim <nurkarim6666@gmail.com>
2024-04-09 10:21:35 +0700

    menambahkan list

--- Karim2.php
+++ Karim2.php
@@ -7,6 +7,11 @@
 </head>
 <body>
     <h1>Wello World</h1>
+    <ul>
+        <li>Izza</li>
+        <li>Papa</li>
+        <li>Mama</li>
+    </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla debitis voluptatibus voluptatum facilis, dolores quisquam neque dolor ut, cupiditate deleniti earum consequatur repudiandae cum et. Ipsum sunt dicta alias quae!</p>
 </body>
 </html>
